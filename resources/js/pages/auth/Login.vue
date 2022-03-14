1be8a50f71151bde2b5a27980da22f28906c60ff
Tobias Schulz <tobias@byte.software>
2022-03-14 12:29:04 +0100

    updated templates

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+<div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 p-4">
     <router-link :to="{name: 'Login'}" class="flex items-center space-x-1 pt-8 sm:justify-start sm:pt-0 text-xl text-gray-800 font-bold">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
@@ -7,7 +7,7 @@
         <span>Laravel Sanctum Vue SPA</span>
     </router-link>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-100 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-100 shadow-md overflow-hidden rounded-lg">
         <form @submit.prevent="login">
             <div>
                 <label class="block font-medium text-sm text-gray-500" for="email">
@@ -61,7 +61,7 @@ export default {
                     .then((response) => {
                         store.dispatch('attempt_user')
                             .then(() => {
-                                router.push({name: 'Home'})
+                                router.replace({name: 'Home'})
                             })
                     })
                     .catch((error) => {
--- webpack.mix.js
+++ webpack.mix.js
@@ -17,3 +17,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
+
+if (mix.inProduction()) {
+   mix.version();
+}
