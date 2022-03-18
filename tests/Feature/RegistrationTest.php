f0aa141b08404fa628db587496b06adfb47a445e
Tobias Schulz <tobias@byte.software>
2022-03-18 12:53:36 +0100

    more basic tests

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_new_users_can_register()
+    {
+        $response = $this->postJson('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+
+        $response->assertCreated();
+    }
+}
