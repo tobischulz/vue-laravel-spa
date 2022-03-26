c38c2a3596c1fc7e2de3c5f321a493c630400775
Tobias Schulz <tobias@byte.software>
2022-03-26 12:24:09 +0100

    fixed two factor challenge colors

--- resources/js/pages/auth/TwoFactorChallenge.vue
+++ resources/js/pages/auth/TwoFactorChallenge.vue
@@ -1,56 +1,56 @@
 <template>
-<div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 p-4">
-    <router-link :to="{name: 'Login'}" class="flex items-center space-x-1 pt-8 sm:justify-start sm:pt-0 text-xl text-gray-800 font-bold">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
-        </svg>
-        <span>Laravel Sanctum Vue SPA</span>
-    </router-link>
+  <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 p-4">
+    <logo></logo>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-100 shadow-md overflow-hidden rounded-lg">
-        <form @submit.prevent="login">
-            <div>
-                <label class="block font-medium text-sm text-gray-500" for="code">
-                    TwoFactor Code
-                </label>
-                <input v-model="code" class="p-2 rounded-md shadow-sm bg-white border-gray-900 text-gray-400 block mt-1 w-full" id="code" type="text" name="code" required="required" autofocus="autofocus">
-            </div>
-            <div v-if="errors" class="text-red-500 py-2 font-semibold">
-                <span>{{ errors.message }}</span>
-            </div>
-            <div class="flex items-center justify-end mt-4">
-                <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3">
-                    Log in
-                </button>
-            </div>
-        </form>
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
+      <form @submit.prevent="login">
+        <div>
+          <label class="block font-medium text-sm text-gray-500" for="code">
+            TwoFactor Code
+          </label>
+          <input v-model="code" class="p-2 rounded-md shadow-sm bg-white border border-gray-300 text-gray-400 block mt-1 w-full" id="code" type="text" name="code" required="required" autofocus="autofocus">
+        </div>
+        <div v-if="errors" class="text-red-500 py-2 font-semibold">
+          <span>{{ errors.message }}</span>
+        </div>
+        <div class="flex items-center justify-end mt-4">
+          <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3">
+            Log in
+          </button>
+        </div>
+      </form>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            code: null,
-            errors: null,
-        }
-    },
-    methods: {
-        login() {
-            axios.post('/two-factor-challenge', {code: this.code, recovery_code: this.code})
-                .then((response) => {
-                    if(response.status !== 204) return
+import Logo from '@/js/components/Logo'
 
-                    this.$store.dispatch('attempt_user')
-                        .then(() => {
-                            this.$router.replace({name: 'Home'})
-                        })
-                })
-                .catch((error) => {
-                    this.errors = error.response.data
-                })
-        }
+export default {
+  data() {
+    return {
+      code: null,
+      errors: null,
     }
+  },
+  components: {
+    Logo,
+  },
+  methods: {
+    login() {
+      axios.post('/two-factor-challenge', {code: this.code, recovery_code: this.code})
+        .then((response) => {
+          if(response.status !== 204) return
+
+          this.$store.dispatch('attempt_user')
+            .then(() => {
+              this.$router.replace({name: 'Home'})
+            })
+        })
+        .catch((error) => {
+          this.errors = error.response.data
+        })
+    }
+  }
 }
 </script>
