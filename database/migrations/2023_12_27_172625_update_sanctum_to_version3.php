cb80331351c82a70d7573db75919d6b9a6e9142d
Tobias Schulz <tobias@byte.software>
2024-01-01 15:32:30 +0100

    upgrade to laravel 10

---
+++ database/migrations/2023_12_27_172625_update_sanctum_to_version3.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (! Schema::hasColumn('personal_access_tokens', 'expires_at')) {
+            Schema::table('personal_access_tokens', function (Blueprint $table) {
+                $table->timestamp('expires_at')->nullable()->after('last_used_at');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
