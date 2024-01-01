27e14fe3ad2f70653cd8203c60f63d69454692a2
Tobias Schulz <tobias@byte.software>
2024-01-01 15:52:44 +0100

    upgrade spa packages

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,17 +1,18 @@
 <template>
   <nav class="bg-white border-b border-gray-200 p-4 mb-4">
     <div class="container mx-auto flex justify-center md:justify-between">
-      <router-link :to="{name: 'Home'}" class="hidden md:flex text-xl font-semibold place-items-center space-x-2">
+      <router-link :to="{ name: 'Home' }" class="hidden md:flex text-xl font-semibold place-items-center space-x-2">
         <span>Fortify Vue SPA</span>
       </router-link>
       <ul class="list flex items-center text-gray-500 text-sm font-semibold">
         <li>
-          <router-link :to="{name: 'Home'}" class="hover:text-gray-700 p-2 rounded flex items-center space-x-2">
+          <router-link :to="{ name: 'Home' }" class="hover:text-gray-700 p-2 rounded flex items-center space-x-2">
             <span>Home</span>
           </router-link>
         </li>
         <li>
-          <router-link :to="{name: 'User'}" class="hover:text-gray-700 p-2 rounded flex items-center space-x-2 font-semibold">
+          <router-link :to="{ name: 'User' }"
+            class="hover:text-gray-700 p-2 rounded flex items-center space-x-2 font-semibold">
             <span>{{ user?.name }}</span>
           </router-link>
         </li>
@@ -36,7 +37,7 @@ export default {
     logout() {
       this.$store.dispatch('logout')
         .then(() => {
-          this.$router.push({name: 'Login'})
+          this.$router.push({ name: 'Login' })
         })
     }
   }
